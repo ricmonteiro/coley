0d1cd29976fa20356674acb24571c20294c7dda5
ricardo <ricardo_luis19@hotmail.com>
2023-02-17 00:11:29 +0000

    Models established, db connection made and superuser created

--- coleyapp/coleyapi/models.py
+++ coleyapp/coleyapi/models.py
@@ -1,106 +1,98 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, AbstractUser,BaseUserManager
+from django.contrib.auth.models import AbstractUser,UserManager
+
+from django.utils import timezone
 # Create your models here.
 
-
 class Analysis(models.Model):
-    analysis_id = models.IntegerField(primary_key=True)
-    user_id = models.IntegerField()
-    cut = models.ForeignKey('Cut', models.DO_NOTHING)
+    id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey('User', models.DO_NOTHING)
+    cut = models.ForeignKey('Cut', models.DO_NOTHING, null=True)
     date = models.DateTimeField()
     result_xlsx_path = models.FileField(upload_to='uploads/')
-    state = models.TextField()  # This field type is a guess.
+    state = models.JSONField()  # This field type is a guess.
 
     class Meta:
-        managed = False
-        db_table = 'analysis'
+        managed = True
+        
 
 
 class Containers(models.Model):
-    container_id = models.IntegerField(primary_key=True)
     container_name = models.CharField(max_length=255)
-    container_description = models.TextField()  # This field type is a guess.
+    container_description = models.JSONField()  # This field type is a guess.
 
     class Meta:
-        managed = False
-        db_table = 'containers'
+        managed = True
+        
 
 
 class Cut(models.Model):
-    cut_id = models.IntegerField(primary_key=True)
     parent_cut = models.ForeignKey('self', models.DO_NOTHING, blank=True, null=True)
-    cut_number = models.IntegerField()
-    user_id = models.IntegerField()
+    cut_number = models.IntegerField(null=True)
+    user_id = models.ForeignKey('User', models.DO_NOTHING)
     purpose = models.CharField(max_length=255)
     cut_date = models.DateTimeField()
 
     class Meta:
-        managed = False
-        db_table = 'cut'
-
+        managed = True
+        
 
 class Patients(models.Model):
-    patient_id = models.IntegerField(primary_key=True)
     patient_name = models.CharField(max_length=255)
     patient_dob = models.DateField()
     gender = models.CharField(max_length=255)
 
     class Meta:
-        managed = False
-        db_table = 'patients'
+        managed = True
+       
 
 
 class Sample(models.Model):
-    sample_id = models.IntegerField(primary_key=True)
-    user_id = models.IntegerField()
+    user_id = models.ForeignKey('User', models.DO_NOTHING)
     origin = models.CharField(max_length=255)
     patient = models.ForeignKey(Patients, models.DO_NOTHING)
     tumor_type = models.ForeignKey('TumorType', models.DO_NOTHING)
-    entry_date = models.DateTimeField()
+    entry_date = models.DateTimeField(default=timezone.now)
     temperature = models.ForeignKey('Temperature', models.DO_NOTHING)
-    container = models.ForeignKey(Containers, models.DO_NOTHING)
-    location = models.TextField()  # This field type is a guess.
-    cut = models.ForeignKey(Cut, models.DO_NOTHING, blank=True, null=True)
+    container = models.ForeignKey('Containers', models.DO_NOTHING)
+    location = models.JSONField()  # This field type is a guess.
+    cut = models.ForeignKey('Cut', models.DO_NOTHING, blank=True, null=True)
 
     class Meta:
-        managed = False
-        db_table = 'sample'
+        managed = True
+      
 
 
 class Temperature(models.Model):
-    temperature_id = models.IntegerField(primary_key=True)
     temperature_desc = models.CharField(max_length=255)
 
     class Meta:
-        managed = False
-        db_table = 'temperature'
+        managed = True
+        
 
 
 class TumorType(models.Model):
-    tumor_type_id = models.SmallIntegerField(primary_key=True)
     tumor_description = models.CharField(max_length=255)
 
     class Meta:
-        managed = False
-        db_table = 'tumor_type'
+        managed = True
+        
 
 
 class User(AbstractUser):
-    id = models.IntegerField(primary_key=True, unique=True)
-    username = models.CharField(unique=True, max_length=255)
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-    email = models.CharField(max_length=255)
+    username = models.CharField(max_length=255, unique=True, null=False)
+    first_name = models.CharField(max_length=255, blank=True, null=True)
+    last_name = models.CharField(max_length=255, blank=True, null=True)
+    email = models.EmailField(max_length=255)
     password = models.CharField(max_length=255)
-    groups = models.IntegerField()
-    user_permissions = models.IntegerField()
-    is_staff = models.BooleanField()
+    groups = models.IntegerField(blank=True, null=True)
+    user_permissions = models.IntegerField(blank=True, null=True)
+    is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
-    is_superuser = models.BooleanField()
-    last_login = models.DateTimeField()
-    date_joined = models.DateTimeField()
-    USERNAME_FIELD = 'id'
+    is_superuser = models.BooleanField(default=False)
+    last_login = models.DateTimeField(blank=True, null=True)
+    date_joined = models.DateTimeField(default=timezone.now)
 
     class Meta:
-        managed = False
-        db_table = 'user'
+        managed = True
+               
